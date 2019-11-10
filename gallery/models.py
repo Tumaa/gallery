dd39607b855cccd2b079d42c89532db232444de1
tumaa <ramanfatuu@gmail.com>
2019-11-10 22:05:56 +0300

    make correction  to class image in  models.py

--- gallery/models.py
+++ gallery/models.py
@@ -28,8 +28,8 @@ class Image(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=50)
     image_path = models.ImageField(upload_to='photos/')
-    image_location = models.ForeignKey(Location)
-    image_category = models.ForeignKey(Category)
+    image_location = models.ForeignKey(Location,on_delete=models.CASCADE,)
+    image_category = models.ForeignKey(Category,on_delete=models.CASCADE,)
 
     def __str__(self):
         return self.name
