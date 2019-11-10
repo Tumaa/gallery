075c18c7b25a72006bb15ba57c45ca4f6b2a5c5a
tumaa <ramanfatuu@gmail.com>
2019-11-10 21:02:22 +0300

    add urls for gallery

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,23 +1,14 @@
-"""gallery URL Configuration
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+#import a package that looks for a variable called urlpatterns (a list). The package checks if the clients side url matches what's inside the list. If it matches, then it follows that path, then goes to the views py file
+from . import views     #access our views
 
-from django.conf.urls import url,include
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^photos/',include('photos.urls'))
+urlpatterns=[
+    url(r'^$', views.initial, name='initial'),        #use of carets. $- anything before the url s considered. r means when a request is made
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^location/(\w+)', views.specific_location, name='specific_location')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
